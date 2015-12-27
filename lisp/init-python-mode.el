6237d08d4feee900108a57c000b688d468dd7754
Chaohong Hu <huchhong@gmail.com>
2015-12-28 01:26:36 +0800

    add jedi config

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -5,5 +5,14 @@
 
 (require-package 'pip-requirements)
 
+;; auto complete
+(require-package 'jedi)
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;; Type:
+;;     M-x package-install RET jedi RET
+;;     M-x jedi:install-server RET
+;; Then open Python file.
 
 (provide 'init-python-mode)
