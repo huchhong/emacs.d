d91b1c40e15bcc876f9e176d3a0163044950fe8e
Chaohong Hu <huchhong@gmail.com>
2015-12-28 07:40:11 +0800

    auto save last position after close file

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -21,6 +21,10 @@
 (window-numbering-mode 1)
 ;; }}
 
+(setq save-place-file "~/.emacs.d/saveplace") ;; keep my ~/ clean
+(setq-default save-place t)                   ;; activate it for all buffers
+(require 'saveplace)                          ;; get the package
+
 ;; disable menu bar
 (menu-bar-mode -1)
 
