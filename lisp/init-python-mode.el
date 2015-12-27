c1bf5c585b1b614ccff4b092758fdc7b05718b21
Chaohong Hu <huchhong@gmail.com>
2015-12-28 02:07:08 +0800

    add jedi:complete key binding to "C-c TAB"

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -10,6 +10,8 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 
+(add-hook 'python-mode-hook
+          (lambda () (define-key python-mode-map (kbd "C-c TAB") 'jedi:complete)))
 ;; Type:
 ;;     M-x package-install RET jedi RET
 ;;     M-x jedi:install-server RET
