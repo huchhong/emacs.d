4713f041cecd87173b3b926a3c6801b5e8c88af0
Chaohong Hu <huchhong@gmail.com>
2016-01-13 03:55:39 +0800

    add git-gutter binding

--- lisp/init-git-gutter.el
+++ lisp/init-git-gutter.el
@@ -7,6 +7,7 @@
  '(git-gutter:always-show-separator t))
 (set-face-foreground 'git-gutter:separator "yellow")
 
-
+(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
 
 (provide 'init-git-gutter)
