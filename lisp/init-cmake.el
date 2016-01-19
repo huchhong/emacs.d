2990642856740f743c0807eb3d5ea6e2bd967294
Chaohong Hu <huchhong@gmail.com>
2016-01-20 05:20:40 +0800

    update init-cmake config

--- lisp/init-cmake.el
+++ lisp/init-cmake.el
@@ -1,6 +1,5 @@
 (require-package 'cmake-mode)
 
 (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
-(add-to-list 'auto-mode-alist '("CMakeLists\\'" . cmake-mode))
 
 (provide 'init-cmake)
